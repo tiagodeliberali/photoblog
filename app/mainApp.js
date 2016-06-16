accc218d3f73d749696bf6e00eeccfe2464b67ce
tiago.deliberali <tiago.deliberali@gmail.com>
2016-06-15 23:05:27 -0300

    removed mainApp
    removed after refactoring

--- app/mainApp.js
+++
@@ -1,9 +0,0 @@
-﻿
-
-.controller('homeController', ['$scope', function ($scope) {
-    
-}])
-
-.controller('postController', ['$scope', '$routeParams', function ($scope, $routeParams) {
-    $scope.post = $scope.posts[$routeParams.postId];
-}])
