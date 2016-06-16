5e3e5df492a62acbe547246bdd0b5c68b7f9ff22
tiago.deliberali <tiago.deliberali@gmail.com>
2016-06-15 23:04:52 -0300

    Clean directory
    Removing all unnecessary files

---
+++ app/mainApp.js
@@ -0,0 +1,9 @@
+﻿
+
+.controller('homeController', ['$scope', function ($scope) {
+    
+}])
+
+.controller('postController', ['$scope', '$routeParams', function ($scope, $routeParams) {
+    $scope.post = $scope.posts[$routeParams.postId];
+}])
