5ea54edd83e8423a871c437498362923982a642e
tiagodeliberali <tiago.deliberali@gmail.com>
2016-06-23 17:18:02 -0300

    Reorganizing css and js files

--- gulpfile.js
+++ gulpfile.js
@@ -4,8 +4,14 @@ var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 
 gulp.task('concat', function () {
-    return gulp.src(['./app/app.module.js', 'app/**/*.js', './script/functions.min.js', './script/ngGallery.js'])
+    return gulp.src(['./app/app.module.js', 'app/**/*.js', './script/primary_load/functions.min.js', './script/primary_load/ngGallery.js'])
         .pipe(concat('app.js'))
+        .pipe(gulp.dest('./script/'));
+});
+
+gulp.task('concat2', function () {
+    return gulp.src(['./script/secondary_load/slideshow-shortcode.js', './script/secondary_load/devicepx-jetpack.js', './script/secondary_load/flexslider.min.js'])
+        .pipe(concat('secondary_load.js'))
         .pipe(gulp.dest('./script/'));
 });
 
@@ -15,8 +21,14 @@ gulp.task('compress', function () {
         .pipe(gulp.dest('./script'));
 });
 
+gulp.task('compress2', function () {
+    return gulp.src('./script/secondary_load.js')
+        .pipe(uglify())
+        .pipe(gulp.dest('./script'));
+});
+
 gulp.task('uncss', function () {
-    return gulp.src('./style/*.css')
+    return gulp.src('./style/third_party/*.css')
         .pipe(concat('./style/main.css'))
         //.pipe(uncss({
         //    html: ['index.html', 'app/**/*.html']
