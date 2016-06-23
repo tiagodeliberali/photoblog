f10666d48fbcfbf2b6d22477ade80c234a18cacf
tiago.deliberali <tiago.deliberali@gmail.com>
2016-06-22 22:38:16 -0300

    Clean index.html

--- gulpfile.js
+++ gulpfile.js
@@ -3,21 +3,21 @@ var uncss = require('gulp-uncss');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 
-gulp.task('concat', function() {
-  return gulp.src(['./app/app.module.js', 'app/**/*.js'])
-    .pipe(concat('app.js'))
-    .pipe(gulp.dest('./script/'));
+gulp.task('concat', function () {
+    return gulp.src(['./app/app.module.js', 'app/**/*.js'])
+        .pipe(concat('app.js'))
+        .pipe(gulp.dest('./script/'));
 });
 
-gulp.task('compress', function() {
-  return gulp.src('./script/app.js')
-    .pipe(uglify())
-    .pipe(gulp.dest('./script'));
+gulp.task('compress', function () {
+    return gulp.src('./script/app.js')
+        .pipe(uglify())
+        .pipe(gulp.dest('./script'));
 });
 
-gulp.task('uncss', function() {
-  return gulp.src('./style/*.css')
-        .pipe(concat('./result/main.css'))
+gulp.task('uncss', function () {
+    return gulp.src('./style/*.css')
+        .pipe(concat('./style/main.css'))
         //.pipe(uncss({
         //    html: ['index.html', 'app/**/*.html']
         //}))
