c495cfd80e6a2f9c021149e2898e7c43bb018418
tiagodeliberali <tiago.deliberali@gmail.com>
2016-06-27 14:14:07 -0300

    Working on yslow suggestions

--- gulpfile.js
+++ gulpfile.js
@@ -2,34 +2,21 @@ var gulp = require('gulp');
 var uncss = require('gulp-uncss');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
+var cleanCSS = require('gulp-clean-css');
 
 gulp.task('concat', function () {
-    return gulp.src(['./app/app.module.js', 'app/**/*.js', './script/primary_load/functions.min.js', './script/primary_load/ngGallery.js'])
+    return gulp.src(['./app/app.module.js', 'app/**/*.js', './script/primary_load/functions.min.js', './script/primary_load/ngGallery.js', './script/secondary_load/slideshow-shortcode.js', './script/secondary_load/devicepx-jetpack.js', './script/secondary_load/flexslider.min.js'])
         .pipe(concat('app.js'))
-        .pipe(gulp.dest('./script/'));
-});
-
-gulp.task('concat2', function () {
-    return gulp.src(['./script/secondary_load/slideshow-shortcode.js', './script/secondary_load/devicepx-jetpack.js', './script/secondary_load/flexslider.min.js'])
-        .pipe(concat('secondary_load.js'))
-        .pipe(gulp.dest('./script/'));
-});
-
-gulp.task('compress', function () {
-    return gulp.src('./script/app.js')
         .pipe(uglify())
-        .pipe(gulp.dest('./script'));
-});
-
-gulp.task('compress2', function () {
-    return gulp.src('./script/secondary_load.js')
-        .pipe(uglify())
-        .pipe(gulp.dest('./script'));
+        .pipe(gulp.dest('./script/'));
 });
 
 gulp.task('uncss', function () {
     return gulp.src('./style/third_party/*.css')
         .pipe(concat('./style/main.css'))
+        .pipe(cleanCSS({
+            compatibility: 'ie8'
+        }))
         //.pipe(uncss({
         //    html: ['index.html', 'app/**/*.html']
         //}))
