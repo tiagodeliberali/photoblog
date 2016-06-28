f768b643dc2315ec6b3a0160fa4ad8eb130939f5
tiagodeliberali <tiago.deliberali@gmail.com>
2016-06-28 14:30:17 -0300

    Fixed auto scroll and label

--- gulpfile.js
+++ gulpfile.js
@@ -4,14 +4,20 @@ var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var cleanCSS = require('gulp-clean-css');
 
-gulp.task('concat', function () {
+gulp.task('concat', function() {
     return gulp.src(['./app/app.module.js', 'app/**/*.js', './script/primary_load/functions.min.js', './script/primary_load/ngGallery.js', './script/secondary_load/slideshow-shortcode.js', './script/secondary_load/devicepx-jetpack.js', './script/secondary_load/flexslider.min.js'])
         .pipe(concat('app.js'))
         .pipe(uglify())
         .pipe(gulp.dest('./script/'));
 });
 
-gulp.task('uncss', function () {
+gulp.task('concat-debug', function() {
+    return gulp.src(['./app/app.module.js', 'app/**/*.js', './script/primary_load/functions.min.js', './script/primary_load/ngGallery.js', './script/secondary_load/slideshow-shortcode.js', './script/secondary_load/devicepx-jetpack.js', './script/secondary_load/flexslider.min.js'])
+        .pipe(concat('app.js'))
+        .pipe(gulp.dest('./script/'));
+});
+
+gulp.task('uncss', function() {
     return gulp.src('./style/third_party/*.css')
         .pipe(concat('./style/main.css'))
         .pipe(cleanCSS({
