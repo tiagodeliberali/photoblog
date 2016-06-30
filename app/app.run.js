f5eb72743e98390d5c0b131e68414294d62e5d21
tiagodeliberali <tiago.deliberali@gmail.com>
2016-06-30 17:20:50 -0300

    Fixed GA to work with routes

--- app/app.run.js
+++ app/app.run.js
@@ -7,16 +7,20 @@
         .module('photoBlogApp')
         .run(appRun);
 
-    appRun.$inject = ['$rootScope'];
+    appRun.$inject = ['$rootScope', '$location', '$window'];
 
-    function appRun($rootScope) {
+    function appRun($rootScope, $location, $window) {
         rootScope = $rootScope;
+        location = $location;
+        window = $window;
 
         rootScope.$on('$routeChangeSuccess', RouteSuccess);
     }
 
     function RouteSuccess(event, current, previous) {
         rootScope.headerType = current.$$route.headerType;
+
+        window.ga('send', 'pageview', { page: location.url() });
 
         if (current.params.postId) {
             rootScope.headerValue = current.params.postId;
